151358f4801218bd21f7707c10c4e859661abf1c
Varaprasad4127 <90756635+Varaprasad4127@users.noreply.github.com>
2024-05-07 01:03:02 +0530

    "commit 8"

--- CarmaApi/main.tf
+++ CarmaApi/main.tf
@@ -157,18 +157,18 @@ resource "aws_iam_instance_profile" "session-manager" {
   role = aws_iam_role.session-manager.name
 }
 
-# resource "aws_instance" "bastion" {
-#   ami                         = lookup(var.amis, var.region)
-#   instance_type               = var.instance_type
-#   key_name                    = aws_key_pair.terraform-lab.key_name
-#   iam_instance_profile        = aws_iam_instance_profile.session-manager.id
-#   associate_public_ip_address = true
-#   security_groups             = [aws_security_group.ec2.id]
-#   subnet_id                   = aws_subnet.public-subnet-1.id
-#   tags = {
-#     Name = "Bastion"
-#   }
-# }
+resource "aws_instance" "bastion" {
+  ami                         = lookup(var.amis, var.region)
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.terraform-lab.key_name
+  iam_instance_profile        = aws_iam_instance_profile.session-manager.id
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.ec2.id]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  tags = {
+    Name = "Bastion"
+  }
+}
 
 resource "aws_launch_configuration" "ec2" {
   name                        = "${var.ec2_instance_name}-instance"
